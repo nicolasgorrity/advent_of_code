d3d3a715b52fb2ac45d0a0b7679cdce2552f843b
Nicolas <nicolas.gorrity@faurecia.com>
2020-12-21 16:21:25 +0100

    ex18

--- 2020/ex18/ex.py
+++ 2020/ex18/ex.py
@@ -1,5 +1,6 @@
+from collections import deque
 from itertools import takewhile
-from typing import Sequence, Tuple, Callable, Optional
+from typing import Sequence, Tuple, Callable, Optional, Iterator, Iterable
 
 
 def index_brackets(string: str,
@@ -53,17 +54,27 @@ def index_operand(expr: str,
         return start_idx, end_idx
 
 
-def index_operator(expr: str) -> Optional[Tuple[int, int]]:
-    nb_leading_spaces = sum(1 for _ in takewhile(str.isspace, expr))
+def index_operator(expr: str,
+                   operators: Optional[Iterator[str]] = None
+                   ) -> Optional[Tuple[int, int]]:
+    if not operators:
+        operators = {'+', '-', '*', '/'}
 
-    if nb_leading_spaces == len(expr):
+    start_idx = sum(1 for _ in takewhile(
+        lambda char:
+        str.isspace(char) or char not in operators,
+        expr))
+
+    if start_idx == len(expr):
         return None
 
-    start_idx = nb_leading_spaces
-    try:
-        end_idx = nb_leading_spaces + expr[nb_leading_spaces:].index(' ') - 1
-    except ValueError:
-        end_idx = len(expr) - 1
+    if start_idx == len(expr) - 1:
+        return start_idx, start_idx
+
+    end_idx = start_idx + sum(1 for _ in takewhile(lambda char:
+                                                   char in operators,
+                                                   expr[start_idx + 1:]))
+
     return start_idx, end_idx
 
 
@@ -82,7 +93,9 @@ def eval_operator(expr: str) -> Callable:
     raise ValueError(f'Operator {expr} not supported')
 
 
-def eval_next_operand(expr: str) -> Optional[Tuple[int, int]]:
+def eval_next_operand(expr: str,
+                      priority_operators: Optional[Iterable[str]] = None,
+                      ) -> Optional[Tuple[int, Tuple[int, int]]]:
     first_operand_idx = index_operand(expr, brackets='()')
 
     if not first_operand_idx:
@@ -94,63 +107,82 @@ def eval_next_operand(expr: str) -> Optional[Tuple[int, int]]:
     try:
         operand_value = int(operand)
     except ValueError:
-        operand_value = eval_expression(expr[operand_start + 1: operand_end])
+        operand_value = eval_expression(expr[operand_start + 1: operand_end],
+                                        priority_operators)
 
-    return operand_value, operand_end
-
-
-def eval_next_operator(expr: str) -> Optional[Tuple[Callable, int]]:
-    first_operator_idx = index_operator(expr)
-
-    if not first_operator_idx:
-        return None
-
-    operator_start, operator_end = first_operator_idx
-    operator_str = expr[operator_start: operator_end + 1]
-    operator = eval_operator(operator_str)
-
-    return operator, operator_end
+    return operand_value, (operand_start, operand_end)
 
 
-def eval_expression(expr: str) -> int:
+def eval_expression(expr: str,
+                    priority_operators: Optional[Iterable[str]] = None
+                    ) -> int:
     # Parse first operand
-    first_operand_eval = eval_next_operand(expr)
+    first_operand_eval = eval_next_operand(expr, priority_operators)
     if not first_operand_eval:
         raise ValueError(f'Invalid expression {expr}')
-    first_operand_value, first_operand_end = first_operand_eval
+    first_operand_value, (first_operand_start, first_operand_end) \
+        = first_operand_eval
 
-    if first_operand_end + 1 == len(expr):
-        return first_operand_value
+    operators = deque()
+    operands = deque([first_operand_value])
 
-    expr = expr[first_operand_end + 1:]
+    # Parse following operators and operands until a priority operator is found
+    while True:
+        if first_operand_end + 1 == len(expr):
+            break
 
-    # Parse first operator
-    operator_eval = eval_next_operator(expr)
-    if not operator_eval:
-        return first_operand_value
-    operator, operator_end = operator_eval
+        expr = expr[first_operand_end + 1:]
 
-    if operator_end + 1 == len(expr):
-        raise ValueError(f'Invalid expression {expr}')
+        # Parse next operator
+        next_operator_idx = index_operator(expr)
+        if not next_operator_idx:
+            break
 
-    expr = expr[operator_end + 1:]
+        operator_start, operator_end = next_operator_idx
+        operator_str = expr[operator_start: operator_end + 1]
 
-    # Parse second operand
-    second_operand_eval = eval_next_operand(expr)
-    if not second_operand_eval:
-        raise ValueError(f'Invalid expression {expr}')
-    second_operand_value, second_operand_end = second_operand_eval
+        if operator_end == len(expr) - 1:
+            raise ValueError('Invalid expression: '
+                             f'ends by operator {operator_str}')
 
-    # Compute operator result
-    operator_result = operator(first_operand_value, second_operand_value)
+        expr = expr[operator_end + 1:]
 
-    if second_operand_end + 1 == len(expr):
-        return operator_result
+        # Parse next operand
+        operand2_eval = eval_next_operand(expr, priority_operators)
+        if not operand2_eval:
+            raise ValueError('Invalid expression: '
+                             f'ends by operator {operator_str}')
+        operand2_value, (operand2_start, operand2_end) = operand2_eval
 
-    # Reduce expression with new result
-    reduced_expr = f'{operator_result}{expr[second_operand_end + 1:]}'
+        # If operand has priority, execute operation
+        if not priority_operators or operator_str in priority_operators:
+            operand1_value = operands.pop()
+            operator = eval_operator(operator_str)
+            result = operator(operand1_value, operand2_value)
+            operands.append(result)
 
-    return eval_expression(reduced_expr)
+        # Else execute any previous operation not impacted by this operation
+        else:
+            if len(operators) > 0:
+                prev_operand2_value = operands.pop()
+                prev_operand1_value = operands.pop()
+                operator = eval_operator(operators.pop())
+                result = operator(prev_operand1_value, prev_operand2_value)
+                operands.append(result)
+
+            # And store this operation
+            operators.append(operator_str)
+            operands.append(operand2_value)
+
+        first_operand_end = operand2_end
+
+    first_operand = operands.popleft()
+    while len(operators) > 0:
+        operator = eval_operator(operators.popleft())
+        second_operand = operands.popleft()
+        first_operand = operator(first_operand, second_operand)
+
+    return first_operand
 
 
 def ex_a(inputs: Sequence[str]) -> int:
@@ -158,7 +190,9 @@ def ex_a(inputs: Sequence[str]) -> int:
 
 
 def ex_b(inputs: Sequence[str]) -> int:
-    return 0
+    return sum(map(lambda expr:
+                   eval_expression(expr, priority_operators={'+', '-'}),
+                   inputs))
 
 
 if __name__ == '__main__':
--- 2020/ex18/test.py
+++ 2020/ex18/test.py
@@ -6,16 +6,22 @@ class Test(unittest.TestCase):
 
     def test_ex_a(self):
         inputs = [
+            '1 + 2 * 3 + 4 * 5 + 6',
+            '1 + (2 * 3) + (4 * (5 + 6))',
             '2 * 3 + (4 * 5)',
             '5 + (8 * 3 + 9 + 3 * 4 * 3)',
             '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
-            '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'
+            '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2',
+            '1 + 2 + 3'
         ]
         outputs = [
+            71,
+            51,
             26,
             437,
             12240,
-            13632
+            13632,
+            6
         ]
 
         for input_data, output in zip(inputs, outputs):
@@ -32,6 +38,29 @@ class Test(unittest.TestCase):
         self.assertEqual(index_brackets('(()'), None)
 
         self.assertEqual(index_brackets('b(k([{j]})(4)2)'), (1, 14))
+
+    def test_ex_b(self):
+        inputs = [
+            '1 + (2 * 3) + (4 * (5 + 6))',
+            '2 * 3 + (4 * 5)',
+            '5 + (8 * 3 + 9 + 3 * 4 * 3)',
+            '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
+            '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2',
+            '1 + 2 + 3'
+        ]
+        outputs = [
+            51,
+            46,
+            1445,
+            669060,
+            23340,
+            6
+        ]
+
+        for input_data, output in zip(inputs, outputs):
+            self.assertEqual(ex_b([input_data]), output)
+
+        self.assertEqual(ex_b(inputs), sum(outputs))
 
 
 if __name__ == '__main__':
