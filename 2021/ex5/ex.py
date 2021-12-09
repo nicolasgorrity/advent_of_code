c4d1a189a79a21dd9a98d0b2034bee4ba7e2976f
Nicolas GORRITY <nicolas.gorrity@gmail.com>
2021-12-09 09:35:32 +0100

    2021 ex5 template

---
+++ 2021/ex5/ex.py
@@ -0,0 +1,24 @@
+import numpy as np
+from typing import List, Sequence
+
+
+def parse_inputs(
+    inputs: Sequence[str],
+) -> Tuple[List[int], np.array]:
+
+    raise NotImplementedError
+
+
+def ex(
+    inputs: Sequence[str],
+) -> int:
+    _ = parse_inputs(inputs=inputs)
+
+
+if __name__ == '__main__':
+    input_file = 'input.txt'
+
+    with open(input_file, 'r') as f:
+        input_data = [line.strip() for line in f.readlines()]
+        print('result a is', ex(input_data))
+        print('result b is', ex(input_data))
---
+++ 2021/ex5/test.py
@@ -0,0 +1,23 @@
+from ex import ex
+
+
+def test_data_ex_a():
+    test_file = 'test.txt'
+
+    with open(test_file, 'r') as f:
+        input_data = [line.strip() for line in f.readlines()]
+
+        result = ex(inputs=input_data)
+
+    assert result == 5
+
+
+def test_data_ex_b():
+    test_file = 'test.txt'
+
+    with open(test_file, 'r') as f:
+        input_data = [line.strip() for line in f.readlines()]
+
+        result = ex(inputs=input_data, pick_first=False)
+
+    assert result == 5
